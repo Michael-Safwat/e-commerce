0696be3b316c328b2627ad39436341a888f845e6
SafwatM <mikesafwat34@gmail.com>
2025-06-13 19:02:20 +0300

    refactor: create token wrabber object for the returned token

--- src/main/java/com/academy/e_commerce/service/AuthService.java
+++ src/main/java/com/academy/e_commerce/service/AuthService.java
@@ -1,11 +1,10 @@
 package com.academy.e_commerce.service;
 
 import com.academy.e_commerce.config.security.jwt.JwtProvider;
+import com.academy.e_commerce.config.security.jwt.Token;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class AuthService {
@@ -16,11 +15,7 @@ public class AuthService {
         this.jwtProvider = jwtProvider;
     }
 
-    public Map<String, String> createJwtToken(Authentication authentication) {
-
-        Map<String, String> loginResultMap = new HashMap<>();
-        String token = this.jwtProvider.createToken(authentication);
-        loginResultMap.put("token", token);
-        return loginResultMap;
+    public Token createJwtToken(Authentication authentication) {
+        return new Token(this.jwtProvider.createToken(authentication));
     }
 }
