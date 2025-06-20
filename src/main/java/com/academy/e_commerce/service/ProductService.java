74102ecf0687e1ac41a30a2f18f763dcb9799675
ElBoreiR <reemabdelbary8@gmail.com>
2025-06-21 00:19:03 +0300

    feat(product): add lock on product update

--- src/main/java/com/academy/e_commerce/service/ProductService.java
+++ src/main/java/com/academy/e_commerce/service/ProductService.java
@@ -1,9 +1,11 @@
 package com.academy.e_commerce.service;
 
+import com.academy.e_commerce.advice.BusinessException;
 import com.academy.e_commerce.dto.ProductDTO;
 import com.academy.e_commerce.mapper.ProductMapper;
 import com.academy.e_commerce.model.Product;
 import com.academy.e_commerce.repository.ProductRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -11,9 +13,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -22,14 +21,12 @@ public class ProductService {
     private final ProductRepository productRepository;
 
         public Product createProduct(ProductDTO productDTO) {
-            log.info("Creating new product: {}", productDTO.name());
             Product product = ProductMapper.productDtoToEntity(productDTO);
             return productRepository.save(product);
         }
 
         public Page<Product> getAllProductsFiltered(String category, String name, Pageable pageable) {
-            log.info("Fetching products with filters: category={}, name={}", category, name);
-
+            log.debug("Fetching products with filters: category={}, name={}", category, name);
             Page<Product> products;
             boolean hasCategory = StringUtils.hasText(category);
             boolean hasName = StringUtils.hasText(name);
@@ -47,23 +44,27 @@ public class ProductService {
         }
 
         public Page<Product> getAllProducts(Pageable pageable) {
-            log.info("Fetching all products with pagination");
             return productRepository.findAll(pageable);
         }
 
         public Product getProductById(Long id) {
-            log.info("Fetching product by ID: {}", id);
+            log.debug("Fetching product by ID: {}", id);
             return productRepository.findById(id)
-                    .orElseThrow(() -> new RuntimeException("Product not found for ID: " + id));
+                    .orElseThrow(() -> new BusinessException("Product not found for ID: " + id));
         }
 
+    @Transactional
         public Product updateProduct(Long id, ProductDTO updatedProductDTO) {
-            log.info("Updating product ID: {}", id);
-            return productRepository.findById(id).map(product -> {
-                Product updatedProduct = ProductMapper.productDtoToEntity(updatedProductDTO);
-                updatedProduct.setId(product.getId());
-                return productRepository.save(updatedProduct);
-            }).orElseThrow(() -> new RuntimeException("Product not found"));
+            log.debug("Updating product ID: {}", id);
+            Product product = productRepository.findByIdWithLock(id).orElseThrow(() -> new BusinessException("Product not found for ID: " + id));
+            product.setName(updatedProductDTO.name());
+            product.setDescription(updatedProductDTO.description());
+            product.setStock(updatedProductDTO.stock());
+            product.setPrice(updatedProductDTO.price());
+            product.setCategory(updatedProductDTO.category());
+            product.setImage(updatedProductDTO.image());
+            product.setRating(updatedProductDTO.rating());
+        return productRepository.save(product);
         }
 
         public void deleteProduct(Long id) {
